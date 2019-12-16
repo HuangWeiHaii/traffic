d38b49d7833404b285e831bc8088d01775765705
1014755647@qq.com <1014755647@qq.com>
2019-12-17 00:49:53 +0800

    update

--- app/src/main/java/com/hwh/traffic/activity/BusInfoActivity.java
+++ app/src/main/java/com/hwh/traffic/activity/BusInfoActivity.java
@@ -1,7 +1,9 @@
 package com.hwh.traffic.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +16,19 @@ import com.hwh.traffic.busEntity.BusDomJson;
 import com.hwh.traffic.busEntity.Stops;
 import com.hwh.traffic.utils.DensityUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class BusInfoActivity extends AppCompatActivity {
+public class BusInfoActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     private LinearLayout bus_info_list;
+    private List<Integer> linearLayout_list = new ArrayList<>();
+    private List<Integer> imageView_point_list = new ArrayList<>();
+    private List<Integer> imageView_bus_list = new ArrayList<>();
+    private List<Integer> textView_list = new ArrayList<>();
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,47 +43,132 @@ public class BusInfoActivity extends AppCompatActivity {
         System.out.println("站点数："+stop_num);
 
         bus_info_list = findViewById(R.id.bus_info_list);
-        int count = bus_info_list.getChildCount();
-        System.out.println("bus_list----"+count);
 
         for (Stops stops : stopsList) {
 
             //显示一条路线
             LinearLayout mLinearLayout = new LinearLayout(getApplicationContext());
-            ImageView mImageView = new ImageView(getApplicationContext());
+            ImageView mImageView_point = new ImageView(getApplicationContext());
+            ImageView imageView_small_bus = new ImageView(getApplicationContext());
+
             TextView mTextView = new TextView(getApplicationContext());
             int dp_10 = DensityUtil.dip2px(getApplicationContext(),10);
 
             bus_info_list.addView(mLinearLayout);
-            mLinearLayout.addView(mImageView);
+            mLinearLayout.addView(mImageView_point);
             mLinearLayout.addView(mTextView);
+            mLinearLayout.addView(imageView_small_bus);
 
             ViewGroup.LayoutParams lp_LinearLayout = mLinearLayout.getLayoutParams();
             lp_LinearLayout.width = ViewGroup.LayoutParams.MATCH_PARENT;
             lp_LinearLayout.height = ViewGroup.LayoutParams.WRAP_CONTENT;
             mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            mLinearLayout.setId(View.generateViewId());
+            int linearLayout_id = mLinearLayout.getId();
+            linearLayout_list.add(linearLayout_id);
             mLinearLayout.setLayoutParams(lp_LinearLayout);
 
-            ViewGroup.LayoutParams lp_ImageView = mImageView.getLayoutParams();
-            lp_ImageView.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-            lp_ImageView.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-            mImageView.setImageResource(R.drawable.point);
-            mImageView.setPadding(dp_10,dp_10,dp_10,dp_10);
-            mImageView.setLayoutParams(lp_ImageView);
+            ViewGroup.LayoutParams lp_ImageView_point = mImageView_point.getLayoutParams();
+            lp_ImageView_point.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+            lp_ImageView_point.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            mImageView_point.setImageResource(R.drawable.point);
+            mImageView_point.setPadding(dp_10,dp_10,dp_10,dp_10);
+            mImageView_point.setId(View.generateViewId());
+            int imageView_point_id = mImageView_point.getId();
+            imageView_point_list.add(imageView_point_id);
+            mImageView_point.setLayoutParams(lp_ImageView_point);
 
-            ViewGroup.LayoutParams lp_TextView = mTextView.getLayoutParams();
+
+            LinearLayout.LayoutParams lp_TextView = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1.0f);
+//            ViewGroup.LayoutParams lp_TextView = mTextView.getLayoutParams();
             lp_TextView.height = ViewGroup.LayoutParams.MATCH_PARENT;
             lp_TextView.width = ViewGroup.LayoutParams.MATCH_PARENT;
             mTextView.setText(stops.getStopName());
             mTextView.setTextColor(getResources().getColor(R.color.black));
             mTextView.setTextSize(20);
             mTextView.setPadding(dp_10,dp_10,dp_10,dp_10);
+            mTextView.setId(View.generateViewId());
+            mTextView.setOnClickListener(this);
+            int textView_id = mTextView.getId();
+            textView_list.add(textView_id);
             mTextView.setLayoutParams(lp_TextView);
+
+
+
+            // bus 站点
+            ViewGroup.LayoutParams lp_ImageView_bus = imageView_small_bus.getLayoutParams();
+            lp_ImageView_bus.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+            lp_ImageView_bus.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+//            imageView_small_bus.setImageResource(R.drawable.small_bus);
+            imageView_small_bus.setPadding(dp_10,dp_10,dp_10,dp_10);
+            imageView_small_bus.setId(View.generateViewId());
+            int imageView_bus_id = imageView_small_bus.getId();
+            imageView_bus_list.add(imageView_bus_id);
+            mImageView_point.setLayoutParams(lp_ImageView_bus);
+
             //显示一条路线
         }
 
+       /* for (Integer integer : linearLayout_list) {
+            System.out.println("linearLayout_list id = " + integer);
+        }
+
+        for (Integer integer : imageView_list) {
+            System.out.println("imageView_list id = " + integer);
+        }
+
+        for (Integer integer : textView_list) {
+            System.out.println("imageView_list id = " + integer);
+        }*/
 
 
+       //假如第3  5  7  9个站点有公交车
+        ImageView small_bus_1 = findViewById(imageView_bus_list.get(3));
+        ImageView small_bus_2 = findViewById(imageView_bus_list.get(5));
+        ImageView small_bus_3 = findViewById(imageView_bus_list.get(7));
+        ImageView small_bus_4 = findViewById(imageView_bus_list.get(9));
+        small_bus_1.setImageResource(R.drawable.small_bus);
+        small_bus_2.setImageResource(R.drawable.small_bus);
+        small_bus_3.setImageResource(R.drawable.small_bus);
+        small_bus_4.setImageResource(R.drawable.small_bus);
 
+
+        Integer start_id = imageView_point_list.get(0);
+        Integer end_id = imageView_point_list.get(imageView_point_list.size()-1);
+        ImageView start_img = findViewById(start_id);
+        ImageView end_img = findViewById(end_id);
+        start_img.setImageResource(R.drawable.bus_start);
+        end_img.setImageResource(R.drawable.bus_end);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //返回的时候需要clear 三个列表
+        linearLayout_list.clear();
+        imageView_bus_list.clear();
+        imageView_point_list.clear();
+        textView_list.clear();
+    }
+
+    @Override
+    public void onClick(View v) {
+        for (Integer integer : textView_list) {
+            TextView no_select_view = findViewById(integer);
+            no_select_view.setTextColor(getResources().getColor(R.color.black));
+        }
+
+        for (Integer integer : imageView_point_list) {
+            ImageView no_select_point = findViewById(integer);
+            no_select_point.setImageResource(R.drawable.point);
+        }
+        int index = textView_list.indexOf(v.getId());
+        Integer point_select_id = imageView_point_list.get(index);
+        ImageView select_point_image = findViewById(point_select_id);
+        select_point_image.setImageResource(R.drawable.stop_select);
+        TextView select_view = findViewById(v.getId());
+        select_view.setTextColor(getResources().getColor(R.color.orange));
     }
 }
---
+++ app/src/main/java/com/hwh/traffic/utils/HttpUtils.java
@@ -0,0 +1,85 @@
+package com.hwh.traffic.utils;
+
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * 发送请求得到数据信息的接口类
+ */
+public class HttpUtils {
+
+    private static HttpURLConnection conn;
+    private static BufferedReader bfr;
+
+
+    public static String httpPost(String url, String canshu) throws InterruptedException {
+        try {
+
+
+//            CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+            URL ur = new URL(url);
+            conn = (HttpURLConnection) ur.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+            //conn.setInstanceFollowRedirects(true);
+            conn.setConnectTimeout(10 * 1000);
+            conn.setReadTimeout(10 * 1000);
+            conn.setDoInput(true);
+            conn.connect();
+            DataOutputStream osw = new DataOutputStream(conn.getOutputStream());
+            StringBuilder sb = new StringBuilder(canshu);
+            osw.write(sb.toString().getBytes());
+            osw.flush();
+            osw.close();
+            Thread.sleep(2000);
+            InputStream i = conn.getInputStream();
+            bfr = new BufferedReader(new InputStreamReader(i));
+            String line = "";
+            String data = "";
+            while ((line = bfr.readLine()) != null) {
+                data = data + line;
+            }
+            bfr.close();
+            bfr = null;
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+
+    }
+
+
+    public static String httpGet(String url) {
+        try {
+            URL ur = new URL(url);
+            conn = (HttpURLConnection) ur.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            conn.setInstanceFollowRedirects(true);
+            conn.setConnectTimeout(10 * 1000);
+            conn.setReadTimeout(10 * 1000);
+            conn.setDoInput(true);
+            conn.connect();
+            //读取返回数据
+            InputStream i = conn.getInputStream();
+            Thread.sleep(20);
+            bfr = new BufferedReader(new InputStreamReader(i));
+            String line;
+            String data = "";
+            while ((line = bfr.readLine()) != null) {
+                data = data + line;
+            }
+            bfr.close();
+            conn.disconnect();
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
