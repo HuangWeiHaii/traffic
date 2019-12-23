77796edf5911916688b7d3b138a014cf549499ee
hwh <1014755647@qq.com>
2019-12-23 17:02:56 +0800

    other computer

--- app/src/main/java/com/hwh/traffic/MapApplication.java
+++ app/src/main/java/com/hwh/traffic/MapApplication.java
@@ -23,25 +23,10 @@ public class MapApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
-        SDKInitializer.initialize(this);
-        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
-        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
-        SDKInitializer.setCoordType(CoordType.GCJ02);
         //初始化数据库 首次运行会自动创建数据库
         trafficLab = new TrafficLab(this);
-
         //开启定位
-        locateUtil = new LocateUtil(getApplicationContext());
-
-  /*      // 开启定位图层
-        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
-        //配置定位SDK参数
-        initLocation();
-        mLocationClient.registerLocationListener(myListener);    //注册监听函数
-        //开启定位
-        mLocationClient.start();
-        Log.d("百度地图定位application","Application开始定位");*/
+        locateUtil = new LocateUtil(this);
 
     }
 
