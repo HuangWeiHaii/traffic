d0b09d9a916b33a422049f2a2dbcb3f6e4c29cb6
hwh <1014755647@qq.com>
2019-12-23 16:57:09 +0800

    other computer

---
+++ app/src/main/java/com/hwh/traffic/baiduLocation/LocateUtil.java
@@ -0,0 +1,85 @@
+package com.hwh.traffic.baiduLocation;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.model.LatLng;
+
+public class LocateUtil {
+
+    private LatLng latLng = null;
+    private LocationClient mLocationClient = null;
+    private BDLocation bdLocation = null;
+    public LocationListener myListener = new LocateUtil.LocationListener();
+
+    public LocateUtil(Context context){
+        // 开启定位图层
+        mLocationClient = new LocationClient(context);     //声明LocationClient类
+        //配置定位SDK参数
+        initLocation();
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        //开启定位
+        mLocationClient.start();
+        Log.d("百度地图定位application","Application开始定位");
+    }
+    //配置定位SDK参数
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("gcj02");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 5000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);
+        option.setOpenGps(true); // 打开gps
+
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public LocationClient getmLocationClient() {
+        return mLocationClient;
+    }
+
+    public void setmLocationClient(LocationClient mLocationClient) {
+        this.mLocationClient = mLocationClient;
+    }
+
+    public BDLocation getBdLocation() {
+        return bdLocation;
+    }
+
+    public void setBdLocation(BDLocation bdLocation) {
+        this.bdLocation = bdLocation;
+    }
+
+    //实现BDLocationListener接口,BDLocationListener为结果监听接口，异步获取定位结果
+    public class LocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            Log.d("百度地图定位application",latLng.toString());
+        }
+    }
+}
--- app/src/main/java/com/hwh/traffic/baiduLocation/LocationUtil.java
+++
@@ -1,6 +0,0 @@
-package com.hwh.traffic.baiduLocation;
-
-public class LocationUtil {
-
-
-}
